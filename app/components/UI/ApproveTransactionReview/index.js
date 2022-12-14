8e1203fb0273bd4022d7310e76c4bc859062c82a
Sylva Elendu <iamsylvaelendu@gmail.com>
2022-12-15 00:27:39 +0100

    fetch token address, nickname and image

--- app/components/UI/ApproveTransactionReview/index.js
+++ app/components/UI/ApproveTransactionReview/index.js
@@ -187,9 +187,9 @@ class ApproveTransactionReview extends PureComponent {
      */
     setTransactionObject: PropTypes.func,
     /**
-     * Update contract nickname
+     * toggle nickname modal
      */
-    onUpdateContractNickname: PropTypes.func,
+    toggleModal: PropTypes.func,
     /**
      * The saved nickname of the address
      */
@@ -216,6 +216,13 @@ class ApproveTransactionReview extends PureComponent {
      * eip1559 gas object for calculating eip1559 transaction
      */
     eip1559GasObject: PropTypes.object,
+    /**
+     * function to toggle the verify contract details modal
+     */
+    showVerifyContractDetails: PropTypes.func,
+    savedContactListToArray: PropTypes.array,
+    closeVerifyContractDetails: PropTypes.func,
+    shouldVerifyContractDetails: PropTypes.bool,
   };
 
   state = {
@@ -229,7 +236,6 @@ class ApproveTransactionReview extends PureComponent {
     spendLimitCustomValue: undefined,
     ticker: getTicker(this.props.ticker),
     viewDetails: false,
-    showContractDetails: false,
     spenderAddress: '0x...',
     transaction: this.props.transaction,
     token: {},
@@ -403,12 +409,6 @@ class ApproveTransactionReview extends PureComponent {
     this.setState({ viewDetails: !viewDetails });
   };
 
-  showVerifyContractDetails = () => {
-    const { showContractDetails } = this.state;
-    // Analytics
-    this.setState({ showContractDetails: !showContractDetails });
-  };
-
   toggleEditPermission = () => {
     const { editPermissionVisible } = this.state;
     !editPermissionVisible &&
@@ -442,9 +442,9 @@ class ApproveTransactionReview extends PureComponent {
     this.setState({ spendLimitCustomValue: value });
   };
 
-  copyContractAddress = async () => {
+  copyContractAddress = async (address) => {
     const { transaction } = this.props;
-    await ClipboardManager.setString(transaction.to);
+    await ClipboardManager.setString(address || transaction.to);
     this.props.showAlert({
       isVisible: true,
       autodismiss: 1500,
@@ -611,6 +611,7 @@ class ApproveTransactionReview extends PureComponent {
       legacyGasObject,
       eip1559GasObject,
       updateTransactionState,
+      showVerifyContractDetails,
     } = this.props;
     const styles = this.getStyles();
     const isTestNetwork = isTestNet(network);
@@ -680,7 +681,7 @@ class ApproveTransactionReview extends PureComponent {
           </Text>
           <ButtonLink
             variant={TextVariants.sBodyMD}
-            onPress={this.showVerifyContractDetails}
+            onPress={showVerifyContractDetails}
             style={styles.verifyContractLink}
           >
             {strings('confirmation.token_allowance.verify_contract_details')}
@@ -800,30 +801,40 @@ class ApproveTransactionReview extends PureComponent {
   };
 
   renderVerifyContractDetails = () => {
-    const { providerType, nickname, onUpdateContractNickname } = this.props;
+    const {
+      providerType,
+      savedContactListToArray,
+      toggleModal,
+      closeVerifyContractDetails,
+    } = this.props;
     const {
       transaction: { to },
       showBlockExplorerModal,
-      showContractDetails,
+      spenderAddress,
+      token: { symbol },
     } = this.state;
 
     const toggleBlockExplorerModal = () => {
       this.setState({
         showBlockExplorerModal: !showBlockExplorerModal,
-        showContractDetails: !showContractDetails,
       });
     };
 
-    const toggleDisplay = () => onUpdateContractNickname();
+    const showNickname = (address) => {
+      toggleModal(address);
+    };
+
     return (
       <VerifyContractDetails
-        toggleVerifyContractView={this.showVerifyContractDetails}
+        closeVerifyContractView={closeVerifyContractDetails}
         toggleBlockExplorerView={toggleBlockExplorerModal}
-        contractAddress={to}
-        toggleNicknameView={toggleDisplay}
-        contractName={nickname}
+        contractAddress={spenderAddress}
+        tokenAddress={to}
+        showNickname={showNickname}
+        savedContactListToArray={savedContactListToArray}
         copyAddress={this.copyContractAddress}
         providerType={providerType}
+        tokenSymbol={symbol}
       />
     );
   };
@@ -833,14 +844,12 @@ class ApproveTransactionReview extends PureComponent {
     const {
       transaction: { to },
       showBlockExplorerModal,
-      showContractDetails,
     } = this.state;
 
     const styles = this.getStyles();
     const closeModal = () => {
       this.setState({
         showBlockExplorerModal: !showBlockExplorerModal,
-        showContractDetails: !showContractDetails,
       });
     };
     return (
@@ -920,19 +929,15 @@ class ApproveTransactionReview extends PureComponent {
   }
 
   render = () => {
-    const {
-      viewDetails,
-      editPermissionVisible,
-      showContractDetails,
-      showBlockExplorerModal,
-    } = this.state;
-    const { isSigningQRObject } = this.props;
+    const { viewDetails, editPermissionVisible, showBlockExplorerModal } =
+      this.state;
+    const { isSigningQRObject, shouldVerifyContractDetails } = this.props;
 
     return (
       <View>
         {viewDetails
           ? this.renderTransactionReview()
-          : showContractDetails
+          : shouldVerifyContractDetails
           ? this.renderVerifyContractDetails()
           : showBlockExplorerModal
           ? this.renderBlockExplorerView()
--- app/components/Views/ApproveView/Approve/index.js
+++ app/components/Views/ApproveView/Approve/index.js
@@ -142,12 +142,14 @@ class Approve extends PureComponent {
     gasSelected: AppConstants.GAS_OPTIONS.MEDIUM,
     gasSelectedTemp: AppConstants.GAS_OPTIONS.MEDIUM,
     transactionConfirmed: false,
-    addNickname: false,
+    shouldAddNickname: false,
+    shouldVerifyContractDetails: false,
     suggestedGasLimit: undefined,
     eip1559GasObject: {},
     eip1559GasTransaction: {},
     legacyGasObject: {},
     legacyGasTransaction: {},
+    address: '',
   };
 
   computeGasEstimates = (overrideGasLimit, gasEstimateTypeChanged) => {
@@ -201,8 +203,16 @@ class Approve extends PureComponent {
     }
   };
 
-  onUpdateContractNickname = () => {
-    this.setState({ addNickname: !this.state.addNickname });
+  showVerifyContractDetails = () =>
+    this.setState({ shouldVerifyContractDetails: true });
+  closeVerifyContractDetails = () =>
+    this.setState({ shouldVerifyContractDetails: false });
+
+  toggleModal = (val) => {
+    this.setState({
+      shouldAddNickname: !this.state.shouldAddNickname,
+      address: val,
+    });
   };
 
   startPolling = async () => {
@@ -551,6 +561,8 @@ class Approve extends PureComponent {
       eip1559GasTransaction,
       legacyGasObject,
       gasError,
+      address,
+      shouldAddNickname,
     } = this.state;
 
     const {
@@ -582,11 +594,25 @@ class Approve extends PureComponent {
 
     const colors = this.context.colors || mockTheme.colors;
 
-    const addressData = checkIfAddressIsSaved(
+    const savedContactList = checkIfAddressIsSaved(
       addressBook,
       network,
       transaction,
     );
+
+    const savedContactListToArray = Object.values(addressBook).flatMap(
+      (value) => Object.values(value),
+    );
+
+    let addressNickname = '';
+
+    const filteredSavedContactList = savedContactListToArray.filter(
+      (contact) => contact.address === safeToChecksumAddress(address),
+    );
+
+    if (filteredSavedContactList.length > 0) {
+      addressNickname = filteredSavedContactList[0].name;
+    }
 
     if (!transaction.id) return null;
     return (
@@ -595,7 +621,9 @@ class Approve extends PureComponent {
         animationIn="slideInUp"
         animationOut="slideOutDown"
         style={
-          this.state.addNickname ? styles.updateNickView : styles.bottomModal
+          this.state.shouldAddNickname
+            ? styles.updateNickView
+            : styles.bottomModal
         }
         backdropColor={colors.overlay.default}
         backdropOpacity={1}
@@ -607,16 +635,12 @@ class Approve extends PureComponent {
         swipeDirection={'down'}
         propagateSwipe
       >
-        {this.state.addNickname ? (
+        {shouldAddNickname ? (
           <AddNickname
-            onUpdateContractNickname={this.onUpdateContractNickname}
-            contractAddress={transaction.to}
-            nicknameExists={addressData && !!addressData.length}
-            nickname={
-              addressData && addressData.length > 0
-                ? addressData[0].nickname
-                : ''
-            }
+            closeModal={this.toggleModal}
+            address={address}
+            savedContactListToArray={savedContactListToArray}
+            addressNickname={addressNickname}
           />
         ) : (
           <KeyboardAwareScrollView
@@ -641,12 +665,18 @@ class Approve extends PureComponent {
                   animateOnChange={animateOnChange}
                   isAnimating={isAnimating}
                   gasEstimationReady={ready}
+                  savedContactListToArray={savedContactListToArray}
                   transactionConfirmed={transactionConfirmed}
-                  onUpdateContractNickname={this.onUpdateContractNickname}
-                  nicknameExists={addressData && !!addressData.length}
+                  toggleModal={this.toggleModal}
+                  showVerifyContractDetails={this.showVerifyContractDetails}
+                  shouldVerifyContractDetails={
+                    this.state.shouldVerifyContractDetails
+                  }
+                  closeVerifyContractDetails={this.closeVerifyContractDetails}
+                  nicknameExists={savedContactList && !!savedContactList.length}
                   nickname={
-                    addressData && addressData.length > 0
-                      ? addressData[0].nickname
+                    savedContactList && savedContactList.length > 0
+                      ? savedContactList[0].nickname
                       : ''
                   }
                   chainId={chainId}
