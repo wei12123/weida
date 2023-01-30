e94ec818fe80838e83d86f7d4520c58672f36f25
Sylva Elendu <iamsylvaelendu@gmail.com>
2023-01-30 12:43:06 +0100

    removed unused code

--- app/components/Nav/Main/RootRPCMethodsUI.js
+++ app/components/Nav/Main/RootRPCMethodsUI.js
@@ -99,7 +99,7 @@ const RootRPCMethodsUI = (props) => {
     });
   };
 
-  const onUnapprovedMessage = (messageParams, type, origin) => {
+  const onUnapprovedMessage = (messageParams, type) => {
     setCurrentPageMeta(messageParams.meta);
     const signMessageParams = { ...messageParams };
     delete signMessageParams.meta;
